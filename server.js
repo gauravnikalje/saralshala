7fdb9adee9e4d8d3c9e37e8bb3b62dfc3424e615
Sandesh <sandesh30apirl@gmail.com>
2025-09-24 00:24:55 +0530

    feat: redesign home page with main and section slideshows

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const mimeTypes = {
     '.json': 'application/json',
     '.png': 'image/png',
     '.jpg': 'image/jpeg',
+    '.jpeg': 'image/jpeg',
     '.gif': 'image/gif',
     '.svg': 'image/svg+xml',
     '.ico': 'image/x-icon'
